0c576b480b238144db1a3cb51ff928f4f50c7c3e
Alek Novkovski <alek@greatcode2.com>
2023-01-28 17:15:52 +0100

    Prettier reformat

--- working/backend/routes/starredRestaurants.js
+++ working/backend/routes/starredRestaurants.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
+const {v4: uuidv4} = require("uuid");
 const router = express.Router();
 const ALL_RESTAURANTS = require("./restaurants").restaurants;
 
@@ -8,41 +8,41 @@ const ALL_RESTAURANTS = require("./restaurants").restaurants;
  * In a "real" application, this data would be maintained in a database.
  */
 let STARRED_RESTAURANTS = [
-  {
-    id: "a7272cd9-26fb-44b5-8d53-9781f55175a1",
-    restaurantId: "869c848c-7a58-4ed6-ab88-72ee2e8e677c",
-    comment: "Best pho in NYC",
-  },
-  {
-    id: "8df59b21-2152-4f9b-9200-95c19aa88226",
-    restaurantId: "e8036613-4b72-46f6-ab5e-edd2fc7c4fe4",
-    comment: "Their lunch special is the best!",
-  },
+    {
+        id: "a7272cd9-26fb-44b5-8d53-9781f55175a1",
+        restaurantId: "869c848c-7a58-4ed6-ab88-72ee2e8e677c",
+        comment: "Best pho in NYC",
+    },
+    {
+        id: "8df59b21-2152-4f9b-9200-95c19aa88226",
+        restaurantId: "e8036613-4b72-46f6-ab5e-edd2fc7c4fe4",
+        comment: "Their lunch special is the best!",
+    },
 ];
 
 /**
  * Feature 6: Getting the list of all starred restaurants.
  */
 router.get("/", (req, res) => {
-  /**
-   * We need to join our starred data with the all restaurants data to get the names.
-   * Normally this join would happen in the database.
-   */
-  const joinedStarredRestaurants = STARRED_RESTAURANTS.map(
-    (starredRestaurant) => {
-      const restaurant = ALL_RESTAURANTS.find(
-        (restaurant) => restaurant.id === starredRestaurant.restaurantId
-      );
+    /**
+     * We need to join our starred data with the all restaurants data to get the names.
+     * Normally this join would happen in the database.
+     */
+    const joinedStarredRestaurants = STARRED_RESTAURANTS.map(
+        (starredRestaurant) => {
+            const restaurant = ALL_RESTAURANTS.find(
+                (restaurant) => restaurant.id === starredRestaurant.restaurantId
+            );
 
-      return {
-        id: starredRestaurant.id,
-        comment: starredRestaurant.comment,
-        name: restaurant.name,
-      };
-    }
-  );
+            return {
+                id: starredRestaurant.id,
+                comment: starredRestaurant.comment,
+                name: restaurant.name,
+            };
+        }
+    );
 
-  res.json(joinedStarredRestaurants);
+    res.json(joinedStarredRestaurants);
 });
 
 /**
@@ -50,11 +50,9 @@ router.get("/", (req, res) => {
  */
 
 
-
 /**
  * Feature 8: Adding to your list of starred restaurants.
  */
-
 
 
 /**
@@ -65,7 +63,6 @@ router.get("/", (req, res) => {
 /**
  * Feature 10: Updating your comment of a starred restaurant.
  */
-
 
 
 module.exports = router;
